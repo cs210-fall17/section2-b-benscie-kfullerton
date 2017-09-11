8e0790065d38035c65a2c4db091230c67e716601
Benjamin Walters <benscie@email.arizona.edu>
2017-09-11 10:01:11 -0700

    Tabbed things, printed thangs.

--- Hello.java
+++ Hello.java
@@ -12,18 +12,19 @@ public class Hello {
         System.out.println("Lock S-foils in attack position");
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
+                System.out.println("Loosen up!");
 
     }
 
     public static void goldLeader() {
-
+        System.out.println("Loosen up!");
         // Gold Leader should add a print statement that says "Loosen up!"
     
 
     }
 
     public static void redLeader() {
- 	System.out.println("I copy, gold leader!");
+        System.out.println("I copy, gold leader!");
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
 
